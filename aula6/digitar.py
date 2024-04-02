549757e521322411d21966b4609b71559283111b
Julia Deisiele <julia.deisiele@alunos.ifsuldeminas.edu.br>
2024-04-02 14:46:05 -0300

    aula6

---
+++ aula6/digitar.py
@@ -0,0 +1,9 @@
+import pyautogui
+#usuario
+pyautogui.click(680,330, duration=0.5)
+pyautogui.write('2022190011')
+#senha
+pyautogui.click(680,420, duration=0.5)
+pyautogui.write('#Jps98288416')
+#botao
+pyautogui.click(680,480, duration=0.5)
---
+++ aula6/exercicio1.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+pyautogui.moveTo(330,45, duration=0.5)   
+pyautogui.dragRel(-150,0, duration=0.5)
